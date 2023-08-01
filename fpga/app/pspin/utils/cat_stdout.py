f478cb00f51f36ace71a13c69ebdff94652a6999
Pengcheng Xu <xubin990510@gmail.com>
2023-08-01 12:03:18 +0200

    allow clearing of old stale log files

--- fpga/app/pspin/utils/cat_stdout.py
+++ fpga/app/pspin/utils/cat_stdout.py
@@ -9,17 +9,20 @@ parser = ArgumentParser(description='Parse and display PsPIN logs by core.')
 parser.add_argument('--dev', type=str, help='device file to read from', default='/dev/pspin1')
 parser.add_argument('--cluster', type=int, help='cluster id to read from', default=0)
 parser.add_argument('--core', type=int, help='core id to read from', default=0)
-parser.add_argument('--dump-files', type=bool, help='dump all cores output', default=False)
+parser.add_argument('--dump-files', action='store_true', help='dump all cores output')
 parser.add_argument('--prefix', type=str, help='file name prefix for output', default='pspin-stdout-')
+parser.add_argument('--clean', action='store_true', help='remove stale old logs')
 
 args = parser.parse_args()
+
+mode = 'wb' if args.clean else 'ab+'
 
 files = [[None] * NUM_CORES for _ in range(NUM_CLUSTERS)]
 if args.dump_files:
     for cl, co in product(range(NUM_CLUSTERS), range(NUM_CORES)):
         # buffering=0 => no buffering
         # https://stackoverflow.com/questions/3167494/how-often-does-python-flush-to-a-file
-        files[cl][co] = open(f'{args.prefix}{cl}.{co}.log', 'ab+', buffering=0)
+        files[cl][co] = open(f'{args.prefix}{cl}.{co}.log', mode, buffering=0)
 
 print(f'Printing stdout for core {args.cluster}.{args.core}')
 print(f'Dump files: {"yes" if args.dump_files else "no"}')
